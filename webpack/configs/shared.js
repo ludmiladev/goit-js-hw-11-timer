6ee273fdd1a2a59127efcf466d3eb63a6ba55431
ludmiladev <45207186+ludmiladev@users.noreply.github.com>
2020-11-02 08:50:31 +0200

    добавила стили

--- webpack/configs/shared.js
+++ webpack/configs/shared.js
@@ -4,72 +4,66 @@ const WebpackBar = require('webpackbar');
 const paths = require('../utils/paths');
 
 module.exports = env => ({
-  mode: env.mode,
-  context: paths.SRC_DIR,
-  entry: './index.js',
-  output: {
-    path: paths.BUILD_DIR,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: paths.SRC_DIR,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              name: '[path][name].[ext]',
-              limit: 8192,
-              esModule: false,
+    mode: env.mode,
+    context: paths.SRC_DIR,
+    entry: './index.js',
+    output: {
+        path: paths.BUILD_DIR,
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                include: paths.SRC_DIR,
+                use: ['babel-loader'],
             },
-          },
-          'img-loader',
-        ],
-      },
-      {
-        test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
-              limit: 10000,
-              mimetype: 'application/font-woff',
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [{
+                        loader: 'url-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                            limit: 8192,
+                            esModule: false,
+                        },
+                    },
+                    'img-loader',
+                ],
             },
-          },
-        ],
-      },
-      {
-        test: /\.(ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
+            {
+                test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',
+                        limit: 10000,
+                        mimetype: 'application/font-woff',
+                    },
+                }, ],
             },
-          },
+            {
+                test: /\.(ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',
+                    },
+                }, ],
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader',
+            },
+            {
+                test: /\.hbs$/,
+                use: 'handlebars-loader',
+            },
         ],
-      },
-      {
-        test: /\.html$/,
-        use: 'html-loader',
-      },
-      {
-        test: /\.hbs$/,
-        use: 'handlebars-loader',
-      },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new FriendlyErrorsWebpackPlugin(),
+        new WebpackBar(),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new FriendlyErrorsWebpackPlugin(),
-    new WebpackBar(),
-  ],
 });
