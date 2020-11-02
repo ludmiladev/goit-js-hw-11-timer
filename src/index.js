22f3d14e6f8375692c6082efef6aad85e0626155
ludmiladev <45207186+ludmiladev@users.noreply.github.com>
2020-11-02 09:05:03 +0200

    добавила стили

--- src/index.js
+++ src/index.js
@@ -1,45 +1,37 @@
 import './styles.css';
 
-const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-]
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.setTimer = this.setTimer.bind(this);
+  }
+  setTimer() {
+    let targetDate = this.targetDate;
+    let selector = this.selector;
+    const date = function () {
+      let currentDay = Date.now();
+      let time = targetDate.getTime() - currentDay;
+      const days = String(Math.floor(time / (1000 * 60 * 60 * 24))).padStart(2, '0');
+      const hours = String(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, '0');
+      const mins = String(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, '0');
+      const secs = String(Math.floor((time % (1000 * 60)) / 1000)).padStart(2, '0');
+      
+      const wrapper = document.querySelector(`${selector}`)
+      wrapper.querySelector('[data-value="days"]').textContent = `${days}`;
+      wrapper.querySelector('[data-value="hours"]').textContent = `${hours}`;
+      wrapper.querySelector('[data-value="mins"]').textContent = `${mins}`;
+      wrapper.querySelector('[data-value="secs"]').textContent = `${secs}`;
+    }
 
-let backgroundInterval = null;
-
-const start = document.querySelector('[data-action="start"]');
-const stop = document.querySelector('[data-action="stop"]');
-const body = document.querySelector('body');
-
-start.addEventListener('click', onColorSwitchStart);
-stop.addEventListener('click', OnColorSwichStop);
-
-stop.disabled = true;
-
-const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-function bacgroundSwich() {
-    return body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)]
-
-};
-
-function onColorSwitchStart() {
-    backgroundInterval = setInterval(bacgroundSwich, 1000);
-
-    stop.disabled = false;
-    start.disabled = true;
-};
-
-function OnColorSwichStop() {
-    clearInterval(backgroundInterval);
-
-    stop.disabled = true;
-    start.disabled = false;
-
+    setInterval(date, 1000)
+  }
 }
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jan 1, 2021'),
+});;
+
+
+timer.setTimer()
